cc1bb7bc30a23cace965738da13be3a04540ca0e
Chando20737 <chando20@gmail.com>
2026-04-27 19:58:09 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const url = 'https://qnrfzfwvddrgyscnxfhh.supabase.co'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFucmZ6Znd2ZGRyZ3lzY254ZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDAxLTM1MDNjNDg0M2M3NWEyYTEiLCJleHAiOjIwNTk4NzYxLTM1MDNjNDg0M2M3NWEyYTF9'
 
 export const supabase = createClient(url, key, {
   auth: {
     persistSession: true,
     storageKey: 'sb-grenier-auth-token',
-    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
     autoRefreshToken: true,
     detectSessionInUrl: true
   }
 })
 
-export const supabaseAdmin = createClient(url, process.env.SUPABASE_SECRET_KEY || key)
+export const supabaseAdmin = createClient(url, key)
