95f5091ada77fc19687c4d1bb430385c41aaf922
Chando20737 <chando20@gmail.com>
2026-04-27 20:21:23 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const url = 'https://qnrfzfwvddrgyscnxfhh.supabase.co'
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFucmZ6Znd2ZGRyZ3lzY254ZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDAxLTM1MDNjNDg0M2M3NWEyYTEiLCJleHAiOjIwNTk4NzYxLTM1MDNjNDg0M2M3NWEyYTF9'
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFucmZ6Znd2ZGRyZ3lzY254ZmhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMTczOTUsImV4cCI6MjA5Mjg5MzM5NX0.bxiwa54-SECHk8361Gt0MZjV7ktAmtAh67GuKMkHvYI'
 
 export const supabase = createClient(url, key, {
   auth: {
